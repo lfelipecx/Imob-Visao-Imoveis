251f4b5dd6b3209932d9cc2ad54ae7ec3b6bf6e2
Luiz Felipe <lfelipecx@gmail.com>
2024-10-01 11:45:00 -0300

    fix: include key component analytics

--- app/_analytics/index.tsx
+++ app/_analytics/index.tsx
@@ -2,14 +2,14 @@ import React from "react"
 
 const Analytics = () => (
     <>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-2678Y3GXQH`} />
         <script
         dangerouslySetInnerHTML={{
             __html: `
                 window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${process.env.GOOGLE_ANALYTICS}', {page_path: window.location.pathname})
+                    gtag('config', 'G-2678Y3GXQH', {page_path: window.location.pathname})
                 `             
             
         }}
