fb582eda6a79ff4646a0059af4c34b9b28f7a59b
Luiz Felipe <lfelipecx@gmail.com>
2026-01-21 11:16:29 -0300

    fix: downgrade prisma version

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,7 +1,19 @@
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@/generated/prisma/client'; 
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-const prisma = new PrismaClient({ adapter })
+declare global {
+  
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+
+  prisma = global.cachedPrisma;
+}
 
 export const db = prisma;
